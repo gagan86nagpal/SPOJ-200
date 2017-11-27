d6ecc55d03a4b70caf3c890d19b881a588548c1d
gagan86nagpal <gagannagpal68@gmail.com>
2017-11-27 13:09:39 +0530

    EIGHTS

---
+++ EIGHTS/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long k;
+        cin>>k;
+        cout<<192+(k-1)*250<<"\n";
+    }
+    return 0;
+}
