7d47404b5dcbda87efd1a94316b12e393e9cf856
gagan86nagpal <gagannagpal68@gmail.com>
2017-12-01 08:11:06 +0530

    NGM

---
+++ NGM/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%10==0)
+        cout<<2<<"\n";
+    else
+        cout<<1<<"\n"<<n%10<<"\n";
+    return 0;
+}
