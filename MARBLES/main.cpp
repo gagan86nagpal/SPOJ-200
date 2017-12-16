059244595c44df72c98a729a60dd892e2e7970a5
gagan86nagpal <gagannagpal68@gmail.com>
2017-12-16 08:33:58 +0530

    MARBLES

---
+++ MARBLES/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+long long C(long long n,long long k)
+{
+    int i;
+    k  = min(n-k,k);
+    long long ans=1;
+    for(i=1;i<=k;i++)
+    {
+        ans*=(n-i+1);
+        ans/=i;
+    }
+    return ans;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long n,k;
+        cin>>n>>k;
+        long long ans = C(n-1,k-1);
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
